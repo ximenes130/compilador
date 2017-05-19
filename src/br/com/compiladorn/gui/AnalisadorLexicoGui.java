130d67829044a3ca6e4138e54c04792e2a63ecc4
ximenes <ximenes130@gmail.com>
2017-05-19 10:04:25 -0300

    Arrumando imports

--- src/br/com/compiladorn/gui/AnalisadorLexicoGui.java
+++ src/br/com/compiladorn/gui/AnalisadorLexicoGui.java
@@ -6,22 +6,15 @@
 package br.com.compiladorn.gui;
 
 import br.com.compiladorn.analisadorlexico.*;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.StringReader;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
-import static javax.xml.transform.OutputKeys.ENCODING;
 
 /**
  *
