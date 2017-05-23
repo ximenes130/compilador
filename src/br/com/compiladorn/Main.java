930133e39a48790ed99963286c4f3b751a6627d7
ximenes <ximenes130@gmail.com>
2017-05-23 20:47:19 -0300

    Comentando codigo gerador de analizador

--- src/br/com/compiladorn/Main.java
+++ src/br/com/compiladorn/Main.java
@@ -5,9 +5,7 @@
  */
 package br.com.compiladorn;
 
-import br.com.compiladorn.analisadorlexico.GeradorAnalisadorLexico;
 import br.com.compiladorn.gui.AnalisadorLexicoGui;
-import java.io.File;
 
 /**
  *
@@ -19,7 +17,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        GeradorAnalisadorLexico.gerar(new File("/home/ximenes/Dev/compiladores/compilador/src/br/com/compiladorn/analisadorlexico/gramatica.lex"));
+        //GeradorAnalisadorLexico.gerar(new File("/home/ximenes/Dev/compiladores/compilador/src/br/com/compiladorn/analisadorlexico/gramatica.lex"));
         
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
